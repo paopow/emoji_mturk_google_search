2383d1bdfd0fc88296394edde1011b4a9a134439
Pao Siangliulue <paopow@seas.harvard.edu>
2013-11-07 07:26:12 -0800

    Show a page page from simple query

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,13 +1,25 @@
 'use strict';
 
 /* Controllers */
+function getSearchUrl(query, start){
+  // TODO: right now query is just one keyword
+  var key = "AIzaSyDGpSyNvR1PEyuWnVn0SPQv_yeXhFdDEmg";
+  var cx = "012731003498980769240%3Adzypydvp2lc";
+  var searchtype = "image";
+  var imgsz = "icon";
+  var url = "https://www.googleapis.com/customsearch/v1?searchType="+searchtype+"&imgsz="+imgsz+"&q="+query+"&start="+start+"&cx="+cx+"&key="+key;
+  return url;
+}
 
 var appModule = angular.module('myApp.controllers', []);
 
 appModule.controller('TurkImgSelectCtrl', ['$scope','$routeParams','$window', '$http',
   	function($scope,$routeParams,$window, $http){
-    $http.get('static/data/images_url.json').success(function(data) {
-      $scope.images = data;
+
+    var url = getSearchUrl('unicorn',1)
+    console.log(url);
+    $http.get(url).success(function(data) {
+      $scope.images = data.items;
     });
 
 }]);
